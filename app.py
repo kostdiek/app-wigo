95c7d47c96df8ed21a7b2de4e5e49777a86c4096
Karen Ostdiek <kmostdiek@gmail.com>
2019-10-22 15:40:46 -0400

    use imdb and output first movie in search

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ def words():
         response["ERROR"] = "title can't be numeric."
     # Now the user entered a valid name
     else:
-        #ia = IMDb()
+        ia = IMDb()
         #search for the movie title
         #movie_title = ia.search_movie(title)
         #movie_id = movie_title[0].movie_id
